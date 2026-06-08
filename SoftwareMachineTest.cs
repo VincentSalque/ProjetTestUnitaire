4130a97e5df8ed226f150e49ce1e38dbef7ad5d9
VincentSlk <vincent.salque@ynov.com>
2026-06-08 15:15:48 +0200

    copie du git du prof

---
+++ SoftwareMachineTest.cs
@@ -0,0 +1,18 @@
+namespace SoftwareMachineTest
+{
+    public class SoftwareMachineTest
+    {
+        [Fact]
+        public void TestInsérerPièce()
+        {
+            Etant donné une machine a café
+            var machine = new SoftwareMachine();
+
+            // Quand j'insère une pièce
+            machine.InsèrerPièce();
+
+            // Alors la machine doit réagir de manière appropriée
+            //
+        }
+    }
+}
