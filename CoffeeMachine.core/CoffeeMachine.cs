124ce21b9f31f1f0421b6af3b1d0c22d1e69178e
geolex <adrien.lexcellent@free.fr>
2026-06-09 10:15:22 +0200

    Naive draft of MakeACoffeeWithTooMuchMoney test

--- CoffeeMachine.core/CoffeeMachine.cs
+++ CoffeeMachine.core/CoffeeMachine.cs
@@ -5,8 +5,10 @@ public class CoffeeMachine
     public void InsertCoin(ushort amount){
         CoffeeServedAmount = 1;
         CollectedAmountInCents = amount;
+        CashbackAmountInCents = 1;
     }
 
     public ushort CoffeeServedAmount {get; private set;}
     public ushort CollectedAmountInCents { get; private set; }
+    public ushort CashbackAmountInCents { get; private set; }
 }
--- CoffeeMachine.test/UnitTest1.cs
+++ CoffeeMachine.test/UnitTest1.cs
@@ -5,7 +5,7 @@ namespace CoffeeMachine.test;
 public class UnitTest1
 {
     [Fact(DisplayName = "Essaye de faire un café")]
-    public void MakeACoffee()
+    public void MakeACoffeeWithEnoughMoney()
     {
         //GIVEN a coffee machine
         var coffeeMachine = new core.CoffeeMachine();
@@ -16,6 +16,25 @@ public class UnitTest1
         //THEN MakeACoffee() is called on the hardware
         Assert.Equal(1, coffeeMachine.CoffeeServedAmount);
 
+
+        //AND CollectStoredMoney() is called on the hardware
+        Assert.Equal(40, coffeeMachine.CollectedAmountInCents);
+    }
+
+    [Fact(DisplayName = "Insère plus d'argent que nécessaire pour faire un café")]
+    public void MakeACoffeeWithTooMuchMoney()
+    {
+        //GIVEN a coffee machine
+        var coffeeMachine = new core.CoffeeMachine();
+
+        //WHEN notice is given that coins were inserted
+        coffeeMachine.InsertCoin(41);
+
+        //THEN MakeACoffee() is called on the hardware
+        Assert.Equal(1, coffeeMachine.CoffeeServedAmount);
+
+        //AND DropCashback(1) is called on the hardware
+        Assert.Equal(1, coffeeMachine.CashbackAmountInCents);
 
         //AND CollectStoredMoney() is called on the hardware
         Assert.Equal(40, coffeeMachine.CollectedAmountInCents);
