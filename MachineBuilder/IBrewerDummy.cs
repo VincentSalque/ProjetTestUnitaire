be9ee306885991fc1d8473d6f66f20cb9e7b8cd5
VincentSlk <vincent.salque@ynov.com>
2026-06-09 10:47:43 +0200

    Mise en place brewerDummy

---
+++ MachineBuilder/IBrewerDummy.cs
@@ -0,0 +1,37 @@
+using Hardware;
+
+namespace MachineBuilder;
+
+public interface IBrewerDummy : IBrewer
+{
+    public bool MakeACoffee()
+    { 
+        throw new Exception("Dummy"); 
+    }
+
+    public bool TryPullWater()
+    {
+        throw new Exception("Dummy");
+    }   
+
+    public bool PourMilk()
+    {
+        throw new Exception("Dummy");
+    }
+
+    public bool PourWater()
+    {
+        throw new Exception("Dummy");
+    }
+
+    public bool PourSugar()
+    {
+        throw new Exception("Dummy");
+    }
+
+    public bool PourChocolate()
+    {
+        throw new Exception("Dummy");
+    }
+
+}
