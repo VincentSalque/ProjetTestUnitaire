bdf678b698a85f01a418441c633a25d8f4900d4e
VincentSlk <vincent.salque@ynov.com>
2026-06-10 10:31:51 +0200

    [Initialisation] Push du MVP du prof pour commencer le projet

---
+++ MachineACafé/SoftwareMachine.cs
@@ -0,0 +1,35 @@
+﻿using Hardware;
+
+namespace MachineACafé;
+
+public class SoftwareMachine
+{
+    private readonly IBrewer _brewer;
+    private readonly IChangeMachine _changeMachine;
+
+    public SoftwareMachine(IBrewer brewer, IChangeMachine changeMachine)
+    {
+        _brewer = brewer;
+        _changeMachine = changeMachine;
+        _changeMachine.RegisterMoneyInsertedCallback(coin => Insérer(new Coin((ushort) coin)));
+    }
+
+    private void Insérer(Coin somme)
+    {
+        if (somme.ValueInCents < 40)
+        {
+            _changeMachine.FlushStoredMoney();
+            return;
+        }
+
+        try
+        {
+            _brewer.MakeACoffee();
+            _changeMachine.CollectStoredMoney();
+        }
+        catch
+        {
+            _changeMachine.FlushStoredMoney();
+        }
+    }
+}
